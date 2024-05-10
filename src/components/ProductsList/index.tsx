72f1c5ff075b07be02aaeeac23df0dc386351385
MarceloSilveira1709 <marcelosilverag@hotmail.com>
2024-05-10 14:32:25 -0300

    criação do modal , navegação entre paginas e implementação da api da loja

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,4 +1,4 @@
-import Game from "../../models/Game";
+import { Game } from "../../pages/Home";
 import Product from "../Product"
 import { Container, List } from "./styles"
 
@@ -8,25 +8,49 @@ export type Props = {
   games: Game[]
 }
 
-export const ProductsList = ({background, title, games}: Props) => (
+export const formatapreco = (preco = 0) => {
+  return new Intl.NumberFormat('pt-BR',{
+    style: 'currency',
+    currency: 'BRL'
+  }). format(preco)
+}
+export const ProductsList = ({background, title, games}: Props) => {
+  const getGameTags = (game: Game) => {
+    const tags = [];
 
-  <Container background={background}>
-    <div className="container">
-    <h2>{title}</h2>
-    <List>
-      {games.map(game => (
-        <Product
-          key={game.id}
-          category={game.category}
-          description={game.description}
-          image={game.image}
-          infos={game.infos}
-          system={game.system}
-          title={game.title}
-        />
-      ))}
-    </List>
-    </div>
+    if(game.release_date) {
+      tags.push(game.release_date)
+    }
+    if (game.prices.discount){
+      tags.push(`${game.prices.discount}%`)
+    }
+    if (game.prices.current){
+      tags.push(formatapreco(game.prices.current))
+  }
 
-  </Container>
-)
+  return tags
+
+  }
+  return(
+      <Container background={background}>
+        <div className="container">
+        <h2>{title}</h2>
+        <List>
+          {games.map(game => (
+            <li key={game.id}>
+              <Product
+              id={game.id}
+              category={game.details.category}
+              description={game.description}
+              image={game.media.thumbnail}
+              infos={getGameTags(game)}
+              system={game.details.system}
+              title={game.name}
+            />
+            </li>
+          ))}
+        </List>
+        </div>
+      </Container>
+    )
+}
